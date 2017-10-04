aa5b90122b25bc42af2b420a3016fef51a308085
vnlvn <levinivanse@gmail.com>
2017-10-04 19:16:50 +0200

    the tokenizer

--- tokeniser.py
+++ tokeniser.py
@@ -1,20 +1,38 @@
-import sys
 import codecs
+import sys
 
 
-text =  sys.stdin.read()
-text = text.replace('.', ' .')
-text = text.replace('?', ' ?')
-text = text.replace('!', ' !')
-text = text.replace('…', ' …')
-text = text.replace(':', ' :')
-text = text.replace(';', ' ;')
-text = text.replace('(', ' (')
-text = text.replace(')', ' )')
-text = text.replace(' ', '\n')
+with open('segmented_corpus.txt') as f:
+    text = f.readlines()
+#f = codecs.open('tokenized_corpus.txt', 'a', 'utf-8')
+tokenized_text = ''
 
+sent_counter = 1
+for line in text:
+	line = line.strip()
+	if line == '':
+		continue
+	if line == ' ':
+		continue	
+	tokenized_text += '# sent_id = %d\n' % (sent_counter)
+	sent_counter += 1
+	tokenized_text += '# text = %s\n' % (line)
+	line = line.replace('.', ' .')
+	line = line.replace(',', ' ,')
+	line = line.replace('?', ' ?')
+	line = line.replace('!', ' !')
+	line = line.replace('…', ' …')
+	line = line.replace(':', ' :')
+	line = line.replace(';', ' ;')
+	line = line.replace('(', ' (')
+	line = line.replace(')', ' )')
+	line = line.replace('"', ' "')
+	line = line.split(' ')
+	word_counter = 1
+	for i in line:
+		tokenized_text += '%d\t' % (word_counter)
+		tokenized_text += '%s\t_\t_\t_\t_\t_\t_\t_\t_\n' % (i)
+		word_counter += 1
+	tokenized_text += '\n'
 
-f = codecs.open('tokenized_corpus.txt', 'w', 'utf-8')
-f.write(text)
-f.close()
-
+sys.stdout.write(tokenized_text)
