7e36d4ab0ae9cded6b27966411f38ba8b33774a9
vnlvn <levinivanse@gmail.com>
2017-11-01 18:42:16 +0100

    the second version

--- train.py
+++ train.py
@@ -3,18 +3,12 @@ import sys
 
 tokens = []
 pos = []
-# read the line by line
 for line in sys.stdin:
-	#print(line)
 	if '\t' not in line:
 		continue
-	#if 'sent_id = 103' in line:
-	#	break
-	#print(line)
 	row = line.split('\t')
 	if row[4] != '_':
 		tokens.append(row[1])
-		#print(row[4])
 		pos.append(row[4])
 
 
@@ -35,6 +29,7 @@ for line in sys.stdin:
 #         print('')
 
 
+# get a dictionary of pos-tokens pairs and their frequency
 pos_tokens_pairs = []
 for i in range(len(tokens)):
 	pos_tokens_pairs.append(pos[i] + '\t' + tokens[i])
@@ -45,25 +40,33 @@ for i in pos_tokens_pairs:
 		pos_tokens_pairs_freq[i] = 1
 	else: pos_tokens_pairs_freq[i] += 1
 
+# pos frequency
 pos_freq = {}
 for i in pos:
 	if i not in pos_freq:
 		pos_freq[i] = 1
 	else: pos_freq[i] += 1
 
-#print(pos_tokens_pairs_freq)
-
+# tokens frequency
+tokens_freq = {}
+for i in tokens:
+	if i not in tokens_freq:
+		tokens_freq[i] = 1
+	else: tokens_freq[i] += 1
 
 output = '# P\tcount\ttag\tform\n'
 for i in pos_freq:
-	output += str(round(pos_freq[i] / len(tokens)), 2)
+	output += str((pos_freq[i] / len(tokens)))
 	output += '\t'
 	output += str(pos_freq[i])
 	output += '\t' 
 	output += i
 	output += '\t_\n'
 for i in pos_tokens_pairs_freq:
-	#print(pos_tokens_pairs_freq[i])
+	# calculate the P column for the word → tag by dividing the frequency of a word = tag pair
+	# by the total number of instances of the word
+	output += str(pos_tokens_pairs_freq[i] / tokens_freq[i.split('\t')[1]])
+	output += '\t'
 	output += str(pos_tokens_pairs_freq[i])
 	output += '\t' 
 	output += i
@@ -71,12 +74,3 @@ for i in pos_tokens_pairs_freq:
 
 sys.stdout.write(output)
 
-
-#for i in tokens:
-#	sys.stdout.write(i, end = ' ')
-
-#for i in pos:
-#	print(i, end = ' ')
-
-#print(len(tokens))
-#print(len(pos))
